9d4bfd466feed92ff2fc8650dfdbdd1a944c0d1b
Simon Norris <snorris@hillcrestgeo.ca>
2020-03-19 14:21:00 -0700

    add column obs_gt5_ind indicating whether a count of obs for one spp is greater or equal than 5

--- sql/observations_by_wsg.sql
+++ sql/observations_by_wsg.sql
@@ -9,20 +9,32 @@ WITH obs AS
     whse_fish.fiss_fish_obsrvtn_pnt_sp
     WHERE observation_date >= DATE('1990-01-01')
     AND point_type_code = 'Observation'
+),
+
+obs_by_wsg AS
+
+(
+    SELECT
+      wsg.watershed_group_code,
+      count(*) FILTER (WHERE obs.species_code = 'CH') as ch_n,
+      max(observation_date) FILTER (WHERE obs.species_code = 'CH') as ch_latest,
+      count(*) FILTER (WHERE obs.species_code = 'CO') as co_n,
+      max(observation_date) FILTER (WHERE obs.species_code = 'CO') as co_latest,
+      count(*) FILTER (WHERE obs.species_code = 'SK') as sk_n,
+      max(observation_date) FILTER (WHERE obs.species_code = 'SK') as sk_latest,
+      count(*) FILTER (WHERE obs.species_code = 'ST') as st_n,
+      max(observation_date) FILTER (WHERE obs.species_code = 'ST') as st_latest
+    FROM whse_basemapping.fwa_watershed_groups_subdivided wsg
+    LEFT OUTER JOIN  obs
+    ON ST_Within(obs.geom, wsg.geom)
+    GROUP BY wsg.watershed_group_code
+    ORDER BY watershed_group_code
 )
 
 SELECT
-  wsg.watershed_group_code,
-  count(*) FILTER (WHERE obs.species_code = 'CH') as ch_n,
-  max(observation_date) FILTER (WHERE obs.species_code = 'CH') as ch_latest,
-  count(*) FILTER (WHERE obs.species_code = 'CO') as co_n,
-  max(observation_date) FILTER (WHERE obs.species_code = 'CO') as co_latest,
-  count(*) FILTER (WHERE obs.species_code = 'SK') as sk_n,
-  max(observation_date) FILTER (WHERE obs.species_code = 'SK') as sk_latest,
-  count(*) FILTER (WHERE obs.species_code = 'ST') as st_n,
-  max(observation_date) FILTER (WHERE obs.species_code = 'ST') as st_latest
-FROM whse_basemapping.fwa_watershed_groups_subdivided wsg
-LEFT OUTER JOIN  obs
-ON ST_Within(obs.geom, wsg.geom)
-GROUP BY wsg.watershed_group_code
-ORDER BY watershed_group_code;
+ *,
+ CASE
+   WHEN ch_n > 5 OR co_n > 5 OR st_n > 5 OR st_n > 5
+   THEN True
+ END AS n_gt5
+FROM obs_by_wsg

d9c9cc4e8382d8ddccc9b168864a5eb15075da8f
Simon Norris <snorris@hillcrestgeo.ca>
2020-03-19 15:40:22 -0700

    match output csv filename to query

--- report.sh
+++ report.sh
@@ -22,7 +22,7 @@ ogr2ogr \
 psql -f sql/01_create_barriers.sql
 
 # qa watershed groups above barriers
-psql2csv < sql/02_wsg_upstream_of_barriers.sql > outputs/wsg_upstream_of_barriers.csv
+psql2csv < sql/02_barrier_qa.sql > outputs/barrier_qa.csv
 
 # create wsg_upstream_of_barriers table
 psql -f sql/03_wsg_upstream_of_barriers.sql

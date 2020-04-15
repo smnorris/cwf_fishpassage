f1aec288608694a10140a1ce4b702bd920c19c93
Simon Norris <snorris@hillcrestgeo.ca>
2020-04-15 09:21:14 -0700

    reporting - combine lake and reservoir areas, fix linear km

--- 03_wsg_prioritize/sql/10_report.sql
+++ 03_wsg_prioritize/sql/10_report.sql
@@ -12,12 +12,10 @@ ON s.waterbody_key = wb.waterbody_key
 areal AS
 (SELECT
  a.watershed_group_code,
- round((sum(st_area(lk.geom)) / 10000)::numeric, 1) as lake_area_ha_total,
- round((sum(st_area(lk.geom)) FILTER (WHERE a.downstream_barrier_id_15 IS NULL) / 10000)::numeric, 1) as lake_area_ha_accessible15,
+ round((sum(st_area(lk.geom)) + sum(st_area(res.geom)) / 10000)::numeric, 1) as lake_reservoir_area_ha_total,
+ round((sum(st_area(lk.geom)) + sum(st_area(res.geom)) FILTER (WHERE a.downstream_barrier_id_15 IS NULL) / 10000)::numeric, 1) as lake_reservoir_area_ha_accessible15,
  round((sum(st_area(wl.geom)) / 10000)::numeric, 1) as wetland_area_ha_total,
- round((sum(st_area(wl.geom)) FILTER (WHERE a.downstream_barrier_id_15 IS NULL) / 10000)::numeric, 1) as wetland_area_ha_accessible15,
- round((sum(st_area(res.geom)) / 10000)::numeric, 1) as reservoir_area_ha_total,
- round((sum(st_area(res.geom)) FILTER (WHERE a.downstream_barrier_id_15 IS NULL) / 10000)::numeric, 1) as reservoir_area_ha_accessible_15
+ round((sum(st_area(wl.geom)) FILTER (WHERE a.downstream_barrier_id_15 IS NULL) / 10000)::numeric, 1) as wetland_area_ha_accessible15
 FROM distinct_wb a
 LEFT OUTER JOIN whse_basemapping.fwa_lakes_poly lk
 ON a.waterbody_key = lk.waterbody_key
@@ -38,8 +36,7 @@ SELECT
   watershed_group_code,
   round((sum(st_length(s.geom)) / 1000)::numeric, 1) as length_km_total,
   round((sum(st_length(s.geom)) FILTER (WHERE s.downstream_barrier_id_15 IS NULL) / 1000)::numeric, 1) AS length_km_accessible15,
-  round((SUM(ST_Length(s.geom)) FILTER (WHERE s.downstream_barrier_id_15 IS NULL AND wb.waterbody_type IS NULL AND s.edge_type IN (1000,1050,1100,2000,2300) AND s.local_watershed_code IS NOT NULL) / 1000)::numeric, 1) AS stream_km_accessible15
- -- round((SUM(ST_Length(s.geom)) FILTER (WHERE s.downstream_barrier_id_15 IS NULL AND wb.waterbody_type = 'R' AND s.local_watershed_code IS NOT NULL) / 1000)::numeric,1) AS river_km,
+  round((SUM(ST_Length(s.geom)) FILTER (WHERE s.downstream_barrier_id_15 IS NULL AND (wb.waterbody_type IS NULL AND s.edge_type IN (1000,1100,2000,2300)) OR wb.waterbody_type = 'R') / 1000)::numeric, 1) AS stream_km_accessible15
  -- round((SUM(ST_Length(s.geom)) FILTER (WHERE s.downstream_barrier_id_15 IS NULL AND wb.waterbody_type = 'L' AND s.local_watershed_code IS NOT NULL) / 1000)::numeric,1) AS lake_km,
  -- round((SUM(ST_Length(s.geom)) FILTER (WHERE s.downstream_barrier_id_15 IS NULL AND wb.waterbody_type = 'W' AND s.local_watershed_code IS NOT NULL) / 1000)::numeric,1) AS wetland_km,
  -- round((SUM(ST_Length(s.geom)) FILTER (WHERE s.downstream_barrier_id_15 IS NULL AND wb.waterbody_type = 'X' AND s.local_watershed_code IS NOT NULL) / 1000)::numeric,1) AS reservoir_km,
@@ -52,12 +49,10 @@ GROUP BY watershed_group_code
 
 SELECT
 linear.*,
-areal.lake_area_ha_total,
-areal.lake_area_ha_accessible15,
+areal.lake_reservoir_area_ha_total,
+areal.lake_reservoir_area_ha_accessible15,
 areal.wetland_area_ha_total,
-areal.wetland_area_ha_accessible15,
-areal.reservoir_area_ha_total,
-areal.reservoir_area_ha_accessible_15
+areal.wetland_area_ha_accessible15
 from linear
 inner join areal
 ON linear.watershed_group_code = areal.watershed_group_code

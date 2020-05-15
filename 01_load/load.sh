b4b9d753b83c5f5b97a5f72558e8445b7c730ffd
Simon Norris <snorris@hillcrestgeo.ca>
2020-05-15 15:51:27 -0700

    add dfo data download/clean to load script

--- 01_load/load.sh
+++ 01_load/load.sh
@@ -68,3 +68,91 @@ ogr2ogr \
   FISS_FISH_RANGES_SVW
 
 psql -f sql/fish_ranges.sql
+
+
+# DFO SAR data - critical habitat
+tmp="${TEMP:-/tmp}"
+
+wget --trust-server-names -qNP "$tmp" https://dfogis.azureedge.net/CriticalHabitat_HabitatEssentiel.zip
+unzip -qun -d "$tmp" "$tmp/CriticalHabitat_HabitatEssentiel.zip"
+ogr2ogr \
+  -f PostgreSQL \
+  PG:"$PGOGR" \
+  -lco GEOMETRY_NAME=geom \
+  -t_srs EPSG:3005 \
+  -lco GEOMETRY_NAME=geom \
+  -nln cwf.dfo_sara_crithab_2019 \
+  -nlt MULTIPOLYGON \
+  -where "Common_Name_EN IN ('Salish Sucker', 'Nooksack Dace', 'Westslope Cutthroat Trout', 'Bull Trout')" \
+  -overwrite \
+  $tmp/CriticalHabitat_FGP.gdb \
+  DFO_SARA_CritHab_2019_FGP_EN
+
+# DFO SAR - distribution
+wget --trust-server-names -qNP "$tmp" https://pacgis01.dfo-mpo.gc.ca/FGPPublic/DFO_Species_at_Risk_Distribution/Distribution_Repartition.zip
+unzip -qun -d "$tmp" "$tmp/Distribution_Repartition.zip"
+ogr2ogr \
+  -f PostgreSQL \
+  PG:"$PGOGR" \
+  -lco GEOMETRY_NAME=geom \
+  -t_srs EPSG:3005 \
+  -lco GEOMETRY_NAME=geom \
+  -nln cwf.dfo_sara_dist_2019_src \
+  -nlt MULTIPOLYGON \
+  -where "Common_Name_EN IN ('Salish Sucker', 'Nooksack Dace', 'Westslope Cutthroat Trout', 'Bull Trout') AND lead_region = 'Pacific'" \
+  -overwrite \
+  Distribution_FGP.gdb \
+  DFO_SARA_Dist_2019_FGP_EN
+
+# subdivide and conquor the messy data
+psql -f dfo.sql
+
+
+# DFO conservation units
+# provided as shapefiles ¯\_(ツ)_/¯
+wget --trust-server-names -qNP "$tmp" https://pacgis01.dfo-mpo.gc.ca/FGPPublic/Pacific_Salmon_CU/Shape_Files/Chinook_Salmon_CU_Shape.zip
+unzip -qun -d "$tmp" "$tmp/Chinook_Salmon_CU_Shape.zip"
+wget --trust-server-names -qNP "$tmp" https://pacgis01.dfo-mpo.gc.ca/FGPPublic/Pacific_Salmon_CU/Shape_Files/Coho_Salmon_CU_Shape.zip
+unzip -qun -d "$tmp" "$tmp/Coho_Salmon_CU_Shape.zip"
+wget --trust-server-names -qNP "$tmp" https://pacgis01.dfo-mpo.gc.ca/FGPPublic/Pacific_Salmon_CU/Shape_Files/Lake_Type_Sockeye_Salmon_CU_Shape.zip
+unzip -qun -d "$tmp" "$tmp/Lake_Type_Sockeye_Salmon_CU_Shape.zip"
+
+# load to local db, stripping out area/length columns that crash the
+# conversion ಠ╭╮ಠ
+ogr2ogr \
+  -f PostgreSQL \
+  PG:"$PGOGR" \
+  -lco GEOMETRY_NAME=geom \
+  -t_srs EPSG:3005 \
+  -lco GEOMETRY_NAME=geom \
+  -nln cwf.dfo_cu_boundary_chinook \
+  -select CU_NAME,FULL_CU_IN,SP_QUAL,CU_TYPE \
+  -nlt MULTIPOLYGON \
+  -overwrite \
+  "$tmp/Chinook_Salmon_CU_Boundary/Chinook_Salmon_CU_Boundary_En.shp"
+
+# CU boundary fields are not standard! ఠ_ఠ
+ogr2ogr \
+  -f PostgreSQL \
+  PG:"$PGOGR" \
+  -lco GEOMETRY_NAME=geom \
+  -t_srs EPSG:3005 \
+  -lco GEOMETRY_NAME=geom \
+  -nln cwf.dfo_cu_boundary_coho \
+  -nlt MULTIPOLYGON \
+  -sql "SELECT CU_name AS CU_NAME, Full_CU_IN as FULL_CU_IN, SPECIES_QU AS SP_QUAL, Type AS CU_TYPE from Coho_Salmon_CU_Boundary_En" \
+  -overwrite \
+  "$tmp/Coho_Salmon_CU_Boundary/Coho_Salmon_CU_Boundary_En.shp"
+
+# file names not standardized / more abbrev field name variations 〴⋋_⋌〵
+ogr2ogr \
+  -f PostgreSQL \
+  PG:"$PGOGR" \
+  -lco GEOMETRY_NAME=geom \
+  -t_srs EPSG:3005 \
+  -lco GEOMETRY_NAME=geom \
+  -nln cwf.dfo_cu_boundary_sockeye \
+  -nlt MULTIPOLYGON \
+  -sql 'SELECT CU_name AS CU_NAME, CU_INDEX as FULL_CU_IN, SP_QUAL, CU_TYPE from "Lake Type Sockeye Salmon CU Boundary_En"' \
+  -overwrite \
+  "$tmp/Lake_Type_Sockeye_Salmon_CU_Boundary/Lake Type Sockeye Salmon CU Boundary_En.shp"

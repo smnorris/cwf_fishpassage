2524ab1b859a4cb34a36bbd802d304216e72450b
Simon Norris <snorris@hillcrestgeo.ca>
2020-05-28 16:23:22 -0700

    do not remove gradient barriers by referencing their id, the ids are not constant - use linear_feature_id

--- 03_wsg_prioritize/sql/01_create_barriers_30.sql
+++ 03_wsg_prioritize/sql/01_create_barriers_30.sql
@@ -87,7 +87,7 @@ FROM cwf.gradient_barriers b
 WHERE b.threshold = .30
 AND b.watershed_group_code IN ('CHWK','FRCN','HARR','LFRA','LILL')
 -- spot manual QA, remove gradients created by dams
-AND b.gradient_barrier_id NOT IN (25696)
+AND b.linear_feature_id NOT IN (4035444)
 ORDER BY blue_line_key, round(downstream_route_measure::numeric, 2)
 ON CONFLICT DO NOTHING;
 

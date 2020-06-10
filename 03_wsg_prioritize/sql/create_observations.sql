c27e289eeb03ecefea9a4bb042f751d48c905f32
Simon Norris <snorris@hillcrestgeo.ca>
2020-06-10 11:29:52 -0700

    only use observations for spp noted to occur within group

--- 03_wsg_prioritize/sql/create_observations.sql
+++ 03_wsg_prioritize/sql/create_observations.sql
@@ -10,14 +10,16 @@ SELECT
   e.localcode_ltree,
   e.downstream_route_measure,
   e.watershed_group_code,
-  e.species_ids,
+  e.species_codes,
   (ST_Dump(
       ST_LocateAlong(s.geom, e.downstream_route_measure)
       )
    ).geom::geometry(PointZM, 3005) AS geom
 FROM whse_fish.fiss_fish_obsrvtn_events e
+INNER JOIN cwf.target_watershed_groups wsg
+ON e.watershed_group_code = wsg.watershed_group_code
+AND e.species_codes && wsg.spp_sar
 INNER JOIN whse_basemapping.fwa_stream_networks_sp s
-ON e.linear_feature_id = s.linear_feature_id
-AND species_ids && (SELECT array_agg(species_id) FROM whse_fish.species_cd WHERE code IN ('SK','CH','CO','ST','WCT','BT','ND','SSU'));
+ON e.linear_feature_id = s.linear_feature_id;
 
 CREATE INDEX ON cwf.fish_obsrvtn_events_sar USING GIST (geom);
